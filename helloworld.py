0286419ad1811d44cc097558f785bce698cacf3c
Maggie <Margie Kodeng>
2016-06-15 02:51:00 +0300

    My very first commit on Data Science

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+b=5
+c=6
+#My day one class of python and data science
+print(b)
+hundred_integer=100
+hundred_string="hundred"
+hundred_float=100.5
+print(hundred_integer,hundred_string,hundred_float)
+
