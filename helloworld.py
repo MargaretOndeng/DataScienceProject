2fd52578d7d02cc8d2c0259f5b3256e872e99ffd
Maggie <Margie Kodeng>
2016-06-15 05:01:19 +0300

    array and lists changes

--- helloworld.py
+++ helloworld.py
@@ -6,4 +6,28 @@ hundred_integer=100
 hundred_string="hundred"
 hundred_float=100.5
 print(hundred_integer,hundred_string,hundred_float)
+l = []
 
+print(type(l))
+l.append("January")
+l.append("February")
+print(l)
+l.append("March")
+l.append("April")
+print(l)
+
+years= []
+years.append(2010)
+years.append(2011)
+years.append(2012)
+years.append(2013)
+years.append(2014)
+
+o=[]
+o.append("Jan")
+o.append(5.0)
+o.append(1.0)
+o.append("uary")
+o.append(10)
+
+print(o)
